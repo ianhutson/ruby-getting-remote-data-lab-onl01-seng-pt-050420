16db1d65b96f741503e36d079a68b46aacc1619a
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-09 16:06:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,7 +9,9 @@ def initialize(url)
   @url = url
 end
 
+def get_response_body
 get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+emd
 get_requester.parse_json
 
 end
