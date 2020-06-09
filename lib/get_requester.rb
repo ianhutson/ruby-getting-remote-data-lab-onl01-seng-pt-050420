de6bccaabee914a035abf4a980a97e6134917bde
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-09 16:07:27 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,7 +10,9 @@ def initialize(url)
 end
 
 def get_response_body
-get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
 end
 
 def parse_json
